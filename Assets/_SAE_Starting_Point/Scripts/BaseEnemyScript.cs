f2f39bc8ebab3619f48b67a53994a442a48200c2
SAE1\1031854 <1031854@student.sae.edu.au>
2024-06-19 10:17:53 +1000

    intial commit

---
+++ Assets/_SAE_Starting_Point/Scripts/BaseEnemyScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyScript : MonoBehaviour
+{
+    protected Player playerReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerReference = FindObjectOfType<Player>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (playerReference)
+        {
+            if (Vector3.Distance(transform.position, playerReference.transform.position) < 5)
+            {
+                Debug.Log("Player is close");
+            }
+        }
+    }
+}
---
+++ Assets/_SAE_Starting_Point/Scripts/TurretScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : BaseEnemyScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerReference = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootPlayer();
+    }
+
+
+    protected void ShootPlayer()
+    {
+        if (playerReference)
+        {
+            if (Vector3.Distance(transform.position, playerReference.transform.position) < 10)
+            {
+                Debug.Log("Shoots Player");
+            }
+        }
+    }
+}
