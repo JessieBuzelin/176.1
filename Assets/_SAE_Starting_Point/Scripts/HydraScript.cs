9e3aa1db1e260b6e1f8a4651d5cf15244f2ae283
JessieBuzelinSAE <1031854@student.sae.edu.au>
2024-06-21 10:48:13 +1000

    Hydra bots now seek the player and deal melee damage

--- Assets/_SAE_Starting_Point/Scripts/HydraScript.cs
+++ Assets/_SAE_Starting_Point/Scripts/HydraScript.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class HydraScript : TurretScript
 {
+    public float moveSpeed = 5f;
+    [SerializeField] protected Transform playerLocation;
+    [SerializeField] private float lookSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerReference = FindObjectOfType<Player>();
     }
+
+    public Transform player; // This will hold a reference to the player's Transform component
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        ShootPlayer();
+        LooksAtPlayer(playerLocation.position);
+
+        if (player != null)
+        {
+            // Calculate direction towards the player
+            Vector3 direction = player.position - transform.position;
+            direction.Normalize();  // Normalize to get a unit direction vector
+
+            // Move towards the player
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+    }
+    protected void LooksAtPlayer(Vector3 playerLocation)
+    {
+
+        if (Vector3.Distance(transform.position, playerLocation) < 10)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(playerLocation - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
+        }
+
+    }
+
+    protected void ShootPlayer() // looks at player and shoots
+    {
+        if (playerReference)
+        {
+            if (Vector3.Distance(transform.position, playerReference.transform.position) < 10)
+            {
+                Debug.Log("Shoots Player");
+
+            }
+        }
     }
 }
